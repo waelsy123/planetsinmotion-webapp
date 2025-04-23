eeec25619fd69f74edf05b8ed9967d21d9314f3b
andresgur <andress.gurpide@gmail.com>
2025-04-23 20:14:42 +0200

    Added Jupyter mass

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,8 @@
 export const DaysToSeconds = 24 * 3600;
-export const Rearth = 6378137 // in m
-export const Mearth = 5.9722 * 10**27 // in m
+export const R_earth = 6378137 // in m
+export const M_earth = 5.9722 * 10**27 // in m
 export const R_sun = 6.957e8; // Radius of the Sun in meters
 export const M_sun = 1.989e33; // Mass of the Sun in g
+export const M_J = 1.898e30; // Mass of the Sun in g
 export const AU = 1.496 * 10 ** 11; // in cm
 export const Gcgs = 6.67 * 10 ** -8; // in cm^3/g/s^2
