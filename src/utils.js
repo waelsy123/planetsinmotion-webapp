107cdbb6ddeb0b7de5d521e8967e49856fb0e6e7
andresgur <andress.gurpide@gmail.com>
2025-04-26 17:07:50 +0200

    Removed draw body function

--- src/utils.js
+++ src/utils.js
@@ -4,15 +4,22 @@ export const linspace = (start, stop, num) => {
     return Array.from({ length: num }, (_, i) => start + i * step);
 };
 
+export function darkenColor(hex, percent) {
+    // Remove the '#' if it exists
+    hex = hex.replace(/^#/, '');
 
+    // Parse the hex color into RGB components
+    let r = parseInt(hex.substring(0, 2), 16);
+    let g = parseInt(hex.substring(2, 4), 16);
+    let b = parseInt(hex.substring(4, 6), 16);
 
-export function drawBody(context, canvas, x, y, body, ratio) {
-    // Draw star face on
-    context.save()
-    context.beginPath();
-    context.arc(canvas.width / 2 + x * ratio, 
-        canvas.height / 2  + y * ratio, body._R * ratio, 0, 2 * Math.PI);
-    context.fillStyle = body.color
-    context.fill();
-    context.restore()
+    // Darken each component by the given percentage
+    r = Math.max(0, Math.min(255, Math.floor(r * (1 - percent / 100))));
+    g = Math.max(0, Math.min(255, Math.floor(g * (1 - percent / 100))));
+    b = Math.max(0, Math.min(255, Math.floor(b * (1 - percent / 100))));
+
+    // Convert the RGB components back to a hex string
+    const darkenedHex = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+
+    return darkenedHex;
 }
