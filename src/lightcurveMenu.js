ccf0585c3e61155eeeceee30bf3ea3538a6b7fb5
andresgur <andress.gurpide@gmail.com>
2025-04-28 12:25:54 +0200

    Added tooltips and language handling

--- src/lightcurveMenu.js
+++ src/lightcurveMenu.js
@@ -1,5 +1,6 @@
 import {DaysToSeconds} from './constants.js'
 import {linspace} from './utils.js'
+import { ToolTipLabel } from './toolTipLabel';
 
 
 export class LightcurveMenu {
@@ -11,13 +12,23 @@ export class LightcurveMenu {
 
     }
 
+    setLanguage(translations) {
+        this.datapointsLabel.setLanguage(translations)
+        this.orbitsLabel.setLanguage(translations)
+    }
+
+
 
     initLightcurve(planets) {
-        const datapointsInput = document.getElementById("datapoints");
+        /// Datapoints
+        const datapointsInput = document.getElementById("input-datapoints");
         this.datapoints = parseInt(datapointsInput.value);
+        this.datapointsLabel = new ToolTipLabel("datapoints")
 
+        /// Orbits
         const orbitsInput = document.getElementById("orbits");
         this.orbits = parseInt(orbitsInput.value);
+        this.orbitsLabel = new ToolTipLabel("orbits")
 
         this.calculateTimes(planets);
 
