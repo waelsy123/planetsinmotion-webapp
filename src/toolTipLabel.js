3dc4748106dcaafe8d4849d8485d329de8f634cb
andresgur <andress.gurpide@gmail.com>
2025-04-28 11:22:37 +0200

    Added tooltip label object

---
+++ src/toolTipLabel.js
@@ -0,0 +1,18 @@
+export class ToolTipLabel {
+    constructor(name) {
+
+        this.name = name
+
+        this.toolTipElement = document.getElementById("tooltip-" + name)
+
+        this.label = document.getElementById("label-" + name)
+
+    }
+
+
+    setLanguage(translations) {
+        this.toolTipElement.innerText = translations[this.name + "_tooltip"];
+    }
+
+
+}
