1dff5138d3eb1c098f744122fc1af552fd5487b5
andresgur <andress.gurpide@gmail.com>
2025-05-25 16:06:29 +0200

    Fixed typo in docs

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const DaysToSeconds = 24 * 3600;
 export const R_earth = 6.3781e+08 // in cm
 export const M_earth = 5.97216787e+27 // in m
-export const R_sun = 6.957e10; // Radius of the Sun in meters
+export const R_sun = 6.957e10; // Radius of the Sun in cm
 export const M_sun = 1.989e33; // Mass of the Sun in g
 export const M_J = 1.8981245973360504e+30; // Mass of jupyter in g
 export const R_J = 7149200000.0; // Radius of jupyter in cm
