4038af3a5edc05bfbd835ad05c056f244efab839
andresgur <andress.gurpide@gmail.com>
2025-05-27 13:07:15 +0200

    Added watermark to the recording

--- src/canvasHandler.js
+++ src/canvasHandler.js
@@ -103,7 +103,9 @@ export class CanvasHandler {
         this.canvas.width = this.width * 2;
         this.canvas.height = this.height * 2;
         this.ctx = this.canvas.getContext("2d");
-
+        this.ctx.font = `italic 32px DejaVu Sans`;
+        this.ctx.strokeStyle = "white";
+        this.ctx.lineWidth = 2;
         var recordedChunks = [];
         const stream = this.canvas.captureStream(ms);
         this.mediaRecorder = new MediaRecorder(stream, { mimeType: format });
@@ -142,6 +144,7 @@ export class CanvasHandler {
         img.onload = () => {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
             this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.strokeText("planetsinmotion.com", this.canvas.width - 500, this.canvas.height - 100);
             URL.revokeObjectURL(url);
         };
     
